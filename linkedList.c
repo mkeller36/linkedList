344aae14fcd6922360167213c2a161cc977a2543
mjkeller <michaelkeller1997@gmail.com>
2024-10-30 21:12:19 -0400

    redesigned code base on low level programming video

--- linkedList.c
+++ linkedList.c
@@ -3,24 +3,59 @@
 
 typedef struct{
     int value;
-    struct node * nextNode;
+    void *next;
 }node;
 
-typedef struct{
-    struct node * head;
-    struct node * tail;
-}list;
+node *head = NULL;
 
-/*TODO; make add node function
-        make delete node function
-        make search nodes function
-        */
+void printMenu(void){
+    printf("You have the following options:\n");
+    printf("\t1. Add a node to the list.\n");
+    printf("\t2. Remove a node from the list.\n");
+    printf("\t3. Insert a node to the list\n");
+    printf("\t4. Print your list.\n");
+    printf("\t5. Exit\n");
+}
+
+node *addNode(int value){
+    node *new;
+    if(head ==NULL){
+        new = malloc(sizeof(node));
+        if(new == NULL){
+            return NULL;
+        }
+        new->value = value;
+        head = new;
+        new->next = NULL;
+    }
+    else {
+        new = malloc(sizeof(node));
+        if(new == NULL){
+            return NULL;
+        }
+        new->value = value;
+        new->next = head;
+        head = new;
+    }
+    return new;
+}
 
 int main(){
-    node n1;
-    node n2;
-    n1.value = 1;
-    n1.nextNode = &n2;
+    int option = -1;
+    while(option != 5){
+        printMenu();
+        int num_recieved = scanf("%d", &option);
+        if(num_recieved == 1 && option > 0 && option <= 5){
+            switch(option){
+                case 1:
 
-    return 1;
+                case 2:
+
+                case 3:
+
+                case 4:    
+            }
+        }
+    }
+
 }
